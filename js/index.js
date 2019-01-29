b3e03d43a3c492b7087121fd7f6fd4992850745b
郑朋 <lea21st@qq.com>
2019-01-29 19:55:44 +0800

    新增复制到粘贴板，粘贴板快速生成二维码

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
  //一般直接写在一个js文件中
  layui.use(['element', 'upload'], function () {
-     var element = layui.element;
+     var element = layui.element,
+         layer = layui.layer;
 
-
-     var updateHeight = function () {
-         var height = $('.layui-tab').height();
-         $('.main').css('height', height);
-     }
-
-     element.on('tab(app)', function (data) {
-         updateHeight();
+     element.on('tab(app)', function () {
+         localStorage.setItem('last_qrcode_extention', $(this).attr('lay-id'));
      });
+
+     //获取上次选中
+     var last_lay_id = localStorage.getItem('last_qrcode_extention') || 1;
+     element.tabChange('app', last_lay_id);
 
      var obj = new QRCode(document.getElementById("qrcode-canvas"), {
          text: window.location.href,
@@ -18,17 +17,36 @@
          height: 640,
          correctLevel: QRCode.CorrectLevel.H
      });
-     $('#text').val(window.location.href);
+
+     //访问粘贴板
+     var paste = function () {
+         var textarea = $('<textarea></textarea>');
+         $("#clipboard_text").html(textarea);
+         textarea[0].select();
+         document.execCommand('paste');
+         var string = $.trim(textarea.val());
+         $('#clipboard_text').empty();
+         return string;
+     }
+
+
      $('#text').on('input propertychange', function () {
          var text = $('#text').val();
          obj.clear();
          obj.makeCode(text);
      });
+     $('#text').on('click', function () {
+         $(this).select();
+         document.execCommand('copy');
+         layer.msg("结果已复制", {
+             time: 1000
+         });
+     });
+
 
      //解析
      qrcode.callback = function (text) {
          $('#text-result').val(text || '图片无法识别');
-         updateHeight();
      }
 
      //解析点击
@@ -40,12 +58,26 @@
      $('#qrcode-text').on('input propertychange', function (e) {
          $('#qrcode-img').html('');
          $('#qrcode-img').closest('.layui-form-item').hide();
-         updateHeight();
          var url = $(this).val();
          $.get(url, function () {
              $('#qrcode-img').html('<img src="' + url + '">');
              $('#qrcode-img').closest('.layui-form-item').show();
              $('#qrcode-button').click();
+         });
+     });
+
+     $('#text-result').on('click', function () {
+         $(this).select();
+         document.execCommand('copy');
+         layer.msg("结果已复制", {
+             time: 1000
+         });
+     });
+     $('#qrcode-text').on('click', function () {
+         $(this).select();
+         document.execCommand('copy');
+         layer.msg("结果已复制", {
+             time: 1000
          });
      });
 
@@ -64,4 +96,16 @@
              });
          }
      });
+
+     //禁用右键
+     document.oncontextmenu = function () {
+         return false
+     }
+
+     var text = paste();
+     if (!text) {
+         text = window.location.href;
+     }
+     $('#text').val(text);
+     $('#qrcode-text').val(paste());
  });
